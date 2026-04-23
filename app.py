066c1e0051281fa101f6a533650ab7c1d437216f
Daniel Langley <dlangley812@hotmail.com>
2026-04-23 14:45:22 -0500

    Add tests

--- app.py
+++ app.py
@@ -15,14 +15,22 @@ app.secret_key = 'secret'
 
 @app.route('/')
 def index():
+    if 'board' not in session:
+        session['board'] = {square: '' for square in range(1, 10)}
+    return redirect(url_for('get_board'))
+
+@app.route('/board/new', methods=['POST'])
+def new_board():
     session['board'] = {square: '' for square in range(1, 10)}
     return redirect(url_for('get_board'))
 
 @app.route('/board')
 def get_board():
+    if 'board' not in session:
+        session['board'] = {square: '' for square in range(1, 10)}
     return render_template('board.html', board=session['board'])
 
-@app.route('/board/mark/<square>')
+@app.route('/board/mark/<square>', methods=['POST'])
 def add_marker(square):
     if session['board'][square]:
         flash(f'square {square} is already marked')
--- tests/test_app.py
+++ tests/test_app.py
@@ -17,5 +17,62 @@ class TTTTest(unittest.TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 302)
 
+    def test_index_redirect(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Tic Tac Toe', response.get_data(as_text=True))
+        self.assertNotIn('X', response.get_data(as_text=True))
+
+    def test_board_alone(self):
+        response = self.client.get('/board')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Tic Tac Toe', response.get_data(as_text=True))
+        self.assertNotIn('X', response.get_data(as_text=True))
+        self.assertNotIn(' O ', response.get_data(as_text=True))
+
+    def test_mark_square(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+        mark_response = self.client.post('/board/mark/5', follow_redirects=True)
+        self.assertEqual(mark_response.status_code, 200)
+        self.assertIn('X', mark_response.get_data(as_text=True))
+
+    def test_double_mark(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+        mark_response = self.client.post('/board/mark/5', follow_redirects=True)
+        self.assertEqual(mark_response.status_code, 200)
+        self.assertIn('X', mark_response.get_data(as_text=True))
+
+        mark_response_2 = self.client.post('/board/mark/5', follow_redirects=True)
+        self.assertEqual(mark_response_2.status_code, 200)
+        self.assertIn('square 5 is already marked', 
+                      mark_response_2.get_data(as_text=True))
+        
+    def test_computer_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+        mark_response = self.client.post('/board/mark/5', follow_redirects=True)
+        self.assertEqual(mark_response.status_code, 200)
+        self.assertIn('X', mark_response.get_data(as_text=True))
+        self.assertIn(' O ', mark_response.get_data(as_text=True))
+
+    def test_new_board(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+
+        mark_response = self.client.post('/board/mark/5', follow_redirects=True)
+        self.assertEqual(mark_response.status_code, 200)
+        
+        new_response = self.client.post('/board/new', follow_redirects=True)
+        self.assertEqual(new_response.status_code, 200)
+        self.assertIn('Tic Tac Toe', new_response.get_data(as_text=True))
+        self.assertNotIn('X', new_response.get_data(as_text=True))
+        self.assertNotIn(' O ', new_response.get_data(as_text=True))
+
+
 if __name__ == '__main__':
     unittest.main()
